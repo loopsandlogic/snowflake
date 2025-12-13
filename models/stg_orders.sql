63e45c9a788a51f181d6617cf80c6ed68eb90d56
Pulastya Pandey <pulastyapandey@gmail.com>
2025-12-13 18:12:09 +0000

    Added incremental model

--- models/stg_orders.sql
+++ models/stg_orders.sql
@@ -1,3 +1,9 @@
+{{
+    config(
+        materialized='incremental'
+    )
+}}
+
 select
     id as order_id,
     user_id as customer_id,
@@ -5,3 +11,7 @@ select
     status
 
 from {{ source('jaffle_shop', 'orders') }}
+{% if is_incremental() %}
+    -- this filter will only be applied on an incremental run
+    where order_date > (select max(order_date) from {{ this }}) 
+{% endif %}
